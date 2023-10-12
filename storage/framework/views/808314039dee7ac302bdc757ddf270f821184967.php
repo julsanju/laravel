9e9e73f4b49411b8b0d7c9e872c5ea1fa1fa922a
Rony Valdelamar <ronnyandres120@gmail.com>
2023-10-12 14:47:34 -0500

    mi primer commit

---
+++ storage/framework/views/808314039dee7ac302bdc757ddf270f821184967.php
@@ -0,0 +1,7 @@
+
+
+<?php $__env->startSection('title', __('Page Expired')); ?>
+<?php $__env->startSection('code', '419'); ?>
+<?php $__env->startSection('message', __('Page Expired')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\ronny\Downloads\codecanyon-Q9jOaXmV-booking-core-ultimate-booking-system (1)\booking-core.3.4.0\BookingCore.3.4.0\resources\views/errors/419.blade.php ENDPATH**/ ?>
